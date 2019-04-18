adf48fd305f168eff450bbd8db66e3ceae4aa76a
Erik Flores <erik.flores@colorado.edu>
2019-04-18 14:14:44 -0600

    scrolling function

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    
+    $("a").on('click', function (event) {
+
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            const hash = this.hash;
+
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+
+                window.location.hash = hash;
+            });
+        }
+    });
+    
+    $(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+
+})
